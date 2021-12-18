55c9ae17ca732a3695660c4e67d8add8a6448b30
justakim12 <rlaruddlf711@naver.com>
2021-12-18 22:12:38 +0900

    Combine into one class for OOP practice

--- practice_algorithms/exercises_part2.py
+++ practice_algorithms/exercises_part2.py
@@ -1,8 +1,8 @@
 # https://codingcompiler.com/python-coding-interview-questions-answers/
 
-# Write a program to reverse a number in Python
-class ReverseNumber:
-    def calculate_reverse_number(self, number: int) -> str:
+class MangleNumber:
+    # Write a program to reverse a number in Python
+    def calculate_reverse_number(self, number) -> None:
         reverse = 0
         while number > 0:
             left = number % 10
@@ -10,14 +10,8 @@ class ReverseNumber:
             number = number // 10
         return print(f"The reverse of the number is: {reverse}")
 
-
-reverse_number = ReverseNumber()
-reverse_number.calculate_reverse_number(12356)
-
-
-# Write a program to find the sum of the digits of a number in Python
-class SumOfDigits:
-    def calculate_sum_of_digits(self, number: int) -> int:
+    # Write a program to find the sum of the digits of a number in Python
+    def calculate_sum_of_digits(self, number) -> None:
         total = 0
         while number > 0:
             digits = number % 10
@@ -25,14 +19,8 @@ class SumOfDigits:
             number = number // 10
         return print(f"The sum of the digits of the number is: {total}")
 
-
-sum_of_digits = SumOfDigits()
-sum_of_digits.calculate_sum_of_digits(555)
-
-
-# Write a Python Program to Count the Number of Digits in a Number
-class CountDigits():
-    def count_num_of_digits(self, number):
+    # Write a Python Program to Count the Number of Digits in a Number
+    def count_num_of_digits(self, number) -> None:
         count = 0
         while number > 0:
             count = count + 1
@@ -40,8 +28,7 @@ class CountDigits():
         return print(f"The count of number of the digits of the number is: {count}")
 
 
-count_of_digits = CountDigits()
-count_of_digits.count_num_of_digits(555)
-
-
-
+mangle_number = MangleNumber()
+mangle_number.calculate_reverse_number(1234)
+mangle_number.calculate_sum_of_digits(1234)
+mangle_number.count_num_of_digits(1234)
