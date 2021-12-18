0aa5875e5a5701ac330b9864ecf2c34a7bc3e16c
justakim12 <rlaruddlf711@naver.com>
2021-12-18 21:42:46 +0900

    Change method name and add different exercise

--- practice_algorithms/exercises_part2.py
+++ practice_algorithms/exercises_part2.py
@@ -2,7 +2,7 @@
 
 # Write a program to reverse a number in Python
 class ReverseNumber:
-    def reverse_number(self, number: int) -> str:
+    def calculate_reverse_number(self, number: int) -> str:
         reverse = 0
         while number > 0:
             left = number % 10
@@ -10,5 +10,22 @@ class ReverseNumber:
             number = number // 10
         return print(f"The reverse of the number is: {reverse}")
 
-test = ReverseNumber()
-test.reverse_number(12356)
+reverse_number = ReverseNumber()
+reverse_number.calculate_reverse_number(12356)
+
+# Write a program to find the sum of the digits of a number in Python
+class SumOfDigits:
+    def calculate_sum_of_digits(self, number: int) -> int:
+        total = 0
+        while number > 0:
+            digits = number % 10
+            total = total + digits
+            number = number // 10
+        return print(f"The sum of the digits of the number is: {total}")
+
+sum_of_digits = SumOfDigits()
+sum_of_digits.calculate_sum_of_digits(555)
+
+
+
+
